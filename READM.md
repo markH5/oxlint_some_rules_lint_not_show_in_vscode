845ba39f6c644754f961c80d403cb3ce2c153691
markH5 <chenmarkh@gmail.com>
2025-03-25 14:08:44 +0800

    add screenshot and log

---
+++ READM.md
@@ -0,0 +1,20 @@
+# oxc vscode extensions can not get some problems
+
+
+```bash
+pnpm oxlint -c .oxlintrc.json
+# or
+pnpm oxlint
+# or
+pnpm lint #
+```
+
+![alt text](p.jpg)
+> get it
+
+![alt text](oxc.oxc-vscode-version.jpg)
+> oxc vscode extensions version:0.16.2
+
+![alt text](log.jpg)
+> [Oxc.log](./Oxc.log)
+> copy from C:\Users\user\AppData\Roaming\Code\logs\20250325T140451\window1\exthost\oxc.oxc-vscode\Oxc.log
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 export function main(someName: string): string {
+    // oxlint default is "warn" ,conifg set -> "no-unused-vars": "error"
+    // and oxc vscode extensions can get diag is "error"
+    let foo = 0;
+
     let someText = "";
     let someTextLen = 0;
     if (someName.replaceAll(/[^\u0000-\u00FF]/g, "aa").length > 10) {
+        //                              ^^^^^^^^^^^^^^^^^^^ can diagnosis in cli, but can not get it in vscode extensions
+
         for (const char of someName.split("")) {
+            //                                          VVVVVVVVVVVVVVVVVV also
             someTextLen += char.replaceAll(/[^\u0000-\u00FF]/g, "aa") == "aa"
                 ? 2
                 : 1;
